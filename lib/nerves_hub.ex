4d9ce991889b9638a2dc3cbaa888b7b12beba1a1
Connor Rigby <konnorrigby@gmail.com>
2018-12-05 12:51:50 -0800

    Add error handling to Client behaviour

--- lib/nerves_hub.ex
+++ lib/nerves_hub.ex
@@ -27,14 +27,19 @@ defmodule NervesHub do
   def update_receive() do
     receive do
       # Reboot when FWUP is done applying the update.
-      {:fwup, {:ok, 0, ""}} ->
+      {:fwup, {:ok, 0, message}} ->
         Logger.info("[NervesHub] Firmware download complete")
+        _ = Client.handle_fwup_message(@client, message)
         Nerves.Runtime.reboot()
 
       # Allow client to handle other FWUP message.
       {:fwup, msg} ->
         _ = Client.handle_fwup_message(@client, msg)
         update_receive()
+
+      {:http_error, error} ->
+        _ = Client.handle_error(@client, error)
+        {:error, {:http_error, error}}
 
       # If the HTTP stream finishes before fwup, just
       # Wait for FWUP to finish.
@@ -43,8 +48,9 @@ defmodule NervesHub do
 
       # If the HTTP stream fails with an error,
       # return
-      {:DOWN, _, :process, _, reason} ->
-        {:error, reason}
+      {:DOWN, _, :process, _, error} ->
+        _ = Client.handle_error(@client, error)
+        error
     end
   end
 end
